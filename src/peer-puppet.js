690120209e1e19e13b9b148bc4e9b1b3f68dd1ec
linus zhou <xlin173@foxmail.com>
2020-09-04 12:12:49 +0800

    fix bug

--- src/peer-puppet.js
+++ src/peer-puppet.js
@@ -1,103 +1,90 @@
 // create answer
 // add stream
 // 获取桌面流
+import EventEmitter from 'events'
 const { desktopCapturer, ipcRenderer} = require('electron')
+let peer = new EventEmitter()
 
-const pc = new window.RTCPeerConnection({})
+// 监听到 offer 指令才创建 RTCPeerConnection
+ipcRenderer.on('offer', (e, offer) => {
+    console.log('init pc', offer)
 
-async function getScreenStreamV2() {
-    const sources = await desktopCapturer.getSources({ types: ['screen'] })
-    const stream = await navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: {
-            mandatory: {
-                chromeMediaSource: 'desktop',
-                chromeMediaSourceId: sources[0].id,
-                maxWidth: window.screen.width,
-                maxHeight: window.screen.height,
-            }
-        }
+    const pc = new window.RTCPeerConnection({})
+
+    // 创建响应并且转发
+    createAnswer(offer).then((answer) => {
+        ipcRenderer.send('forward', 'answer', { type: answer.type, sdp: answer.sdp })
     })
-    stream.getTracks().forEach(function (track) {
-        pc.addTrack(track, stream);
-    });
-}
 
-// async function getScreenStream(pc) {
-//     const sources = await desktopCapturer.getSources({ types: ['screen'] })
-//     return new Promise((resolve, reject) => {
-//         navigator.mediaDevices.getUserMedia({
-//             audio: false,
-//             video: {
-//                 mandatory: {
-//                     chromeMediaSource: 'desktop',
-//                     chromeMediaSourceId: sources[0].id,
-//                     maxWidth: window.screen.width,
-//                     maxHeight: window.screen.height,
-//                 }
-//             }
-//         }, (stream) => {
-//             resolve(stream)
-//             // peer.emit('add-stream', stream) // 触发事件
-//         }, (err) => {
-//             console.error(err)
-//         })
-//     })
-// }
+    // ipcRenderer.on('offer', async (e, offer) => {
+    //     let answer = await createAnswer(offer)
+    //     ipcRenderer.send('forward', 'answer', { type: answer.type, sdp: answer.sdp })
+    // })
 
-pc.ondatachannel = (e) => {
-    console.log('datachannel', e)
-    e.channel.onmessage = (e) => {
-        let {type, data} = JSON.parse(e.data)
-        if (type === 'mouse') {
-            data.screen = {
-                width: window.screen.width,
-                height: window.screen.height,
+    //获取视频流
+    async function getScreenStreamV2() {
+        const sources = await desktopCapturer.getSources({ types: ['screen'] })
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: false,
+            video: {
+                mandatory: {
+                    chromeMediaSource: 'desktop',
+                    chromeMediaSourceId: sources[0].id,
+                    maxWidth: window.screen.width,
+                    maxHeight: window.screen.height,
+                }
             }
+        })
+        stream.getTracks().forEach(function (track) {
+            pc.addTrack(track, stream);
+        });
+    }
+
+    pc.onicecandidate = function (e) {
+        let candidate = e.candidate
+        console.log('candidate', JSON.stringify(candidate))
+        if (candidate) {
+            pc.addIceCandidate(candidate)
+            // ipcRenderer.send('forward', 'puppet-candidate', candidate.toJSON())
         }
-        ipcRenderer.send('robot', type, data)
     }
-} 
 
-pc.onicecandidate = function (e) {
-    console.log('candidate', JSON.stringify(e.candidate))
-}
+    // async function addIceCandidate(candidate) {
+    //     if (!candidate || !candidate.type) return
+    //     console.log('addIceCandidate success', candidate)
+    //     await pc.addIceCandidate(new RTCIceCandidate(candidate))
+    // }
 
-pc.onicecandidate = function (e) {
-    let candidate = e.candidate
-    console.log('candidate', JSON.stringify(candidate))
-    if (candidate) {
-        ipcRenderer.send('forward', 'puppet-candidate', candidate.toJSON())
+    // ipcRenderer.on('candidate', (e, candidate) => {
+    //     addIceCandidate(candidate)
+    // })
+
+    async function createAnswer(offer) {
+        // 添加流
+        await getScreenStreamV2()
+        await pc.setRemoteDescription(offer)
+        await pc.setLocalDescription(await pc.createAnswer())
+        console.log('answer', JSON.stringify(pc.localDescription))
+        return pc.localDescription
     }
-}
 
-let candidates = [];
-
-async function addIceCandidate(candidate) {
-    if (candidate) {
-        candidates.push(candidate)
-    }
-    if (pc.remoteDescription && pc.remoteDescription.type) {
-        for (let i = 0; i < candidates.length; i++) {
-            await pc.addIceCandidate(new RTCIceCandidate(candidates[i]))
+    // 接收控制指令
+    pc.ondatachannel = (e) => {
+        console.log('peer-puppet recieve order', e)
+        e.channel.onmessage = (e) => {
+            let { type, data } = JSON.parse(e.data)
+            if (type === 'mouse') {
+                data.screen = {
+                    width: window.screen.width,
+                    height: window.screen.height,
+                }
+            }
+            ipcRenderer.send('robot', type, data)
         }
-        candidates = []
+        e.channel.onerror = (e) => {
+            console.log('peer-puppet channle error', e)
+        }
     }
-}
-window.addIceCandidate = addIceCandidate
-
-ipcRenderer.on('offer', async (e, offer) => {
-    let answer = await createAnswer(offer)
-    ipcRenderer.send('forward', 'answer', {type: answer.type, sdp: answer.sdp})
 })
 
-async function createAnswer(offer) {
-    // 添加流
-    await getScreenStreamV2()
-    await pc.setRemoteDescription(offer)
-    await pc.setLocalDescription(await pc.createAnswer())
-    console.log('answer', JSON.stringify(pc.localDescription))
-    return pc.localDescription
-}
-
-window.createAnswer = createAnswer
+export default peer
